7b7cc67378294149e6338b7d7f66896e16ebd6a3
bachir <contact@creativspeed.com>
2018-09-25 22:14:30 +0100

    exercise 2 - twoFer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -7,7 +7,7 @@ import (
 
 // ShareWith Someone's name passed on @name :string -> returns :string
 func ShareWith(name string) string {
-    if len(strings.TrimSpace(name)) == 0 {
+    if strings.TrimSpace(name) == "" {
         name = "you"
     }
 	return "One for " + name + ", one for me."
